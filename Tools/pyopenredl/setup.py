5f16fcc9018981e971c2944b7c42eecb4b9c93ec
res <res@uol.de>
2024-09-24 23:01:05 +0200

    added setup file

---
+++ Tools/pyopenredl/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+          name="pyopenredl",
+          version="2.0.0",
+         )
