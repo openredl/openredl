1aecbf5fde34dc04a6e1870438e0ff8b4cd4e08b
res <res@uol.de>
2024-09-26 23:54:40 +0200

    improved log plots

--- Tools/example_loadandplot_logs.py
+++ Tools/example_loadandplot_logs.py
@@ -5,87 +5,69 @@ Created on Tue Jul 30 10:53:35 2024
 
 @author: aj
 """
-from pyopenredl.logutils import *
-
+from pyopenredl.logutils import read_data_to_df, plot_data, check_share_x, kwds
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+
     ##############################################################################
     # Configuration
     ##############################################################################
     ## Files to read
     # (with the format [file_marker]YYMMDD[file_extension], e.g. M240726.TXT)
     dir_path = "example_sensors_sampling/ArduinoDL/"
-    file_extention = ".TXT"
-    file_marker = "M"
-
-    # offset to convert datetime from CET to UTC
-    tzoffset =  datetime.timedelta(hours=-2)
-
-    # Date filter
-    t_start = "2024/07/25 15:00:00"
-    t_end = "2024/07/30 8:00:00"
+    # offset to convert datetime from measurements
+    time_correction_hours = 0
+    time_correction_seconds = 0
+    # Date filter (after correction)
+    # format '%Y/%m/%d %H:%M:%S' e.g. "2024/09/26 12:50:00"
+    t_start = "2024/7/26 12:30:00"
+    t_end = "2024/7/30 10:00:00"
 
     # Colums to plot
-    cols_toplot =[ "DIFF1_mean[mV]",
-                  "DIFF3_mean[mV]",
+    cols_toplot =[["DIFF1_mean[mV]", "DIFF1_min[mV]", "DIFF1_max[mV]"],
+                  ["DIFF1_mean[mV]", "DIFF1_min[mV]", "DIFF1_max[mV]"],
                   "V2rms[V]",
+                  "P2real[W]",
+                  "PF2[]",
+                  "f2[Hz]"
                   ]
-
-
+    # Plot Configuration
+    kwds = {
+            #"linestyle": 'None',
+            "marker": ".",
+            "lw": 0.4,
+            "marker": ".",
+            "markersize":0.8
+            }
+            
+    ## Code
     ###########################################################################
-
-    dt_start = datetime.datetime.strptime(t_start,
-                                          '%Y/%m/%d %H:%M:%S')
-    dt_end = datetime.datetime.strptime(t_end,
-                                          '%Y/%m/%d %H:%M:%S')
-
-    ###########################################################################    
     # Reading log files
-    files = get_files_list(dir_path, file_extention, file_marker, t_start, t_end)
-    df = load_files_in_df(files, dir_path, t_start, t_end)
-    df = df.drop('notes>', axis=1)
-    # Time correction
-    df.index = df.index + pd.Timedelta(10, unit='s')
-    # Filtering time
-    df = df[(df.index> t_start) & (df.index< t_end)]
-    # Resampling
-    df_rs15min = df.resample('15min').mean()
+    df = read_data_to_df( dir_path,
+                          t_start,
+                          t_end,
+                          t_correction_h=time_correction_hours,
+                          t_correction_s=time_correction_seconds
+                          )
 
+    # Creating plot
+    nrows = len(cols_toplot) if cols_toplot else len(list(df.columns.values))
+    fig, axes = plt.subplots(ncols=1,
+                             nrows=nrows,
+                             figsize=(12,6),
+                             sharex = check_share_x(cols_toplot),
+                             num = "Data Plot",
+                             )
 
-    ###########################################################################    
     # Plotting
-    ###########################################################################    
-    fig, axes = plt.subplots(ncols=1, nrows=len(cols_toplot),
-                             figsize=(9,9),
-                             sharex = True)
-    fig.tight_layout(rect=[0.02, 0.02, .98, 0.95])
-    axes.flatten()
-
-    lw =0.75
-    mk = ","
-    mk_sz = 2
-    
-    for i in range(len(cols_toplot)):
-        axnum = i
-
-        x = df.index
-        y = df[cols_toplot[i]]
-        axes[axnum].step(x, y, marker = mk, markersize = mk_sz, 
-                         where = 'post',
-                         linewidth = lw,
-                         label = cols_toplot[i],
-                         )
-        x = df_rs15min.index
-        y = df_rs15min[cols_toplot[i]]
-        axes[axnum].step(x, y, marker = mk, markersize = mk_sz, 
-                         where = 'post',
-                         linewidth = lw,
-                         label = cols_toplot[i] + "_15min",
-                         )
-        
-        axes[axnum].legend(loc = 'upper left')
-        axes[axnum].grid()
-
+    data = plot_data(df, fig, axes, cols_to_plot=cols_toplot)
+    # Plotting with resample
+    #data_1min = plot_data(df, fig, axes, cols_to_plot=cols_toplot, resample_interval="1min")
+    # example for resample
+    #df_rs15min = df.resample('15min').mean()
+   
+    fig.tight_layout( )
     plt.show()
     
     ############################################################################
--- Tools/pyopenredl/src/pyopenredl/logutils.py
+++ Tools/pyopenredl/src/pyopenredl/logutils.py
@@ -9,16 +9,17 @@ from io import StringIO
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 from numpy import nan
+from pathlib import Path
 
-
-
+file_extention = ".TXT"
+file_marker = "M"
 
 
 def get_files_list(dir_path = None,
-                   file_extention = ".TXT",
-                   file_marker = "M",
                    t_start = None,
-                   t_end = None
+                   t_end = None,
+                   file_extention = file_extention,
+                   file_marker = file_marker,
                    ):
     """Filename format [file_marker, M]YYMMDD[file_extention, .TXT]"""
     
@@ -32,8 +33,11 @@ def get_files_list(dir_path = None,
 
     
     files = []
-    if dir_path =="": dir_path = None
-    for file in os.listdir(dir_path):
+    if dir_path =="":
+        dir_path_formated = None
+    else:
+        dir_path_formated = Path(dir_path)
+    for file in os.listdir(dir_path_formated):
         if file.endswith(file_extention) and file.startswith(file_marker):
             #print(file)
             year = int(file[1:3])+2000
@@ -50,7 +54,7 @@ def get_files_list(dir_path = None,
 
 
 
-def load_files_in_df(files, path, t_start, t_end):
+def load_files_in_df(files, path, t_start, t_end, t_correction_h=0, t_correction_s=0 ):
     
     df = pd.DataFrame()
     for file in files:
@@ -79,11 +83,148 @@ def load_files_in_df(files, path, t_start, t_end):
                               parse_dates = True)
             df = pd.concat([df, df2], axis = 0)
     
-    df.sort_index(inplace = True)
-    tzoffset =  datetime.timedelta(hours=-2) # offset to convert datetime to UTC
-    df['localdt'] = df.index + tzoffset
-    df.set_index('localdt', inplace = True)
-    df = df[(df.index> t_start) & (df.index< t_end)]
-    #df.index = pd.to_datetime(df.index, unit="s")
+    if len(df):
+        df.sort_index(inplace = True)
+        #tzoffset =  datetime.timedelta(hours=-2) # offset to convert datetime to UTC
+        #df['localdt'] = df.index + tzoffset
+        #df.set_index('localdt', inplace = True)
+        df.index = (df.index +
+                pd.Timedelta(t_correction_s, unit='s') +
+                pd.Timedelta(t_correction_h, unit='h')
+               )
+
+
+        df = df[(df.index> t_start) & (df.index< t_end)]
+        #df.index = pd.to_datetime(df.index, unit="s")
+        # Filtering time
+        df = df[(df.index> t_start) & (df.index< t_end)]
+        if "notes>" in df.columns:
+             df = df.drop('notes>', axis=1)
     
     return df
+    
+def read_data_to_df(dir_path,
+                   t_start=None,
+                   t_end=None,
+                   t_correction_h=0,
+                   t_correction_s=0
+                   ):
+                   
+    files = get_files_list(dir_path = dir_path,
+                     t_start = t_start,
+                     t_end = t_end,
+                     )
+                     
+    df = load_files_in_df(files,
+                     dir_path,
+                     t_start,
+                     t_end,
+                     t_correction_h,
+                     t_correction_s
+                     )
+                     
+    if not len(df):
+        print("No data found for the given time interval.\n")
+    else:
+        print(f"{len(df)} rows found for the given time interval.\n")
+
+
+    return df
+
+
+
+def check_share_x(cols_to_plot):
+    """Check if all plots have default x axis."""
+    if len(cols_to_plot)==0:
+        return True
+    for ax_num, cols in enumerate(cols_to_plot):
+        if type(cols[0]) is dict:
+            return False
+    else:
+        return True
+
+kwds = {#"linestyle": 'None',
+        #"marker": ".",
+        "lw": 0.4,
+        "marker": ".",
+        "markersize":0.8
+        }
+
+
+def plot_data(df, fig, axes, cols_to_plot=None, resample_interval = None, ):
+    
+    if len(cols_to_plot) == 1:
+        axes = [axes]
+    
+    if resample_interval is None:
+        data = df
+        resample_label=""
+    else:
+        data = df.resample(resample_interval).mean()
+        resample_label= "_" + resample_interval
+
+    if cols_to_plot == None or len(cols_to_plot)==0:
+        cols_to_plot = [[c] for c in data.columns]
+    elif len(cols_to_plot) == 0:
+        print("Colums to plot is empty")
+     
+    for ax_num, col_info in enumerate(cols_to_plot):
+        kwds_ax = kwds.copy()
+        x_plot = data.index # index is default x axis
+        
+        
+        if type(col_info[0]) is dict:
+            col_info_tmp = col_info[0].copy()
+            cols = col_info_tmp.pop("y")
+            
+            if "x" in col_info_tmp.keys():
+                x_plot = data[columns[col_info_tmp.pop('x')]]
+
+            kwds_ax.update(col_info_tmp)
+            
+        elif type(col_info) is str:
+            cols = [col_info]
+            
+        elif type(col_info) is int:
+        
+            cols = [col_info]
+        
+        else:
+            cols = col_info
+
+        for col in cols:
+            try:
+                # is the axis given by name?
+                if isinstance(col, str):
+                    y_plot = data[col]
+                    kwds_ax["label"] = col + resample_label
+
+                    
+                # is the axis given by column number?
+                else:
+                    y_plot = data[columns[col]]
+                    kwds_ax["label"] = columns_labels[col] + resample_label
+                
+                axes[ax_num].plot(x_plot,
+                                  y_plot,
+                                   **kwds_ax
+                                       )
+            except IndexError:
+               print("[Warning] Column to plot not found:", col)
+            except KeyError:
+               print("[Warning] Key not found in data", col)
+               
+        for ax in axes:
+            ax.legend(loc='center left',
+                      bbox_to_anchor=(1.01, 0.5),
+                      ncols=1,
+                      fontsize = "small"
+                      )
+            ax.grid(visible=True)
+    #fig.tight_layout( )
+    #plt.show()
+
+               
+               
+    return data
+                           
