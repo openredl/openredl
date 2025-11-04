05c2b2b76239ed70d876eaf333224d55833667bc
res <res@uol.de>
2025-11-04 10:59:31 +0100

    added real_time_plot.py script to firmware examples

---
+++ Firmware/OpenREDL_lib/examples/all_differential_channels/real_time_plot.py.h
@@ -0,0 +1,57 @@
+"""Real Time Plot script template
+   To execute it:
+   - remove the file name extension .h (only necessary for autocopy)
+   - activate the right environment: conda activate openredl-env 
+   - execute the script: python real_time_plot.py
+"""
+
+import pyopenredl.realtimeplotutils as rtp
+
+##########################################################################
+# CONFIGURATION PARAMETERS
+##########################################################################
+
+# Automatic port selection.
+#terms to search automatically for a port.
+terms = ['D307R956', 'arduino', "2341:0043"]
+port = rtp.autoselect_port(terms, 0)
+# Else, select manually the port of the Datalogger in your computers.
+# You can find the right port name in:
+# Arduino Ide > tools > port or in Windows: device manager.
+#port = '/dev/ttyACM0'
+#port = '/dev/rfcomm0'
+#port = 'COM3'
+
+# how many units of data should be plotted
+unit = "seconds" # points/seconds
+amount = 5*60 # how many units should be kept in the plot
+
+# Which data columns should be plotted?
+# Array of subarrays. Each subarray "[]" is a plotting axis.
+# y_axis_n can be given by column_number or column_name
+# column_number starts with column 0 (datetime)
+# Possible subarrays format:
+#       [y_axis1, y_axis2...]
+#       [{"y":[y_axis1, y_axis2...]}]
+#       [{"x":x_axis, "y":[y_axis1, y_axis2...], "<matplotlib_plot_kwd:value}]
+columns_to_plot = [
+   #[1,2,3,4], # example multiple y_axis with column0/datetime as x_axis
+   #[1],  # example one y_axis with column0/datetime as x_axis
+   #[2, "P1real[W]"], # example with y_axis as column number and as column_name
+   #[{"x":2,"y":[2,"P1real[W]"]}], # example with other column as x _axis
+   #[{"x":2,"y":[3], "linestyle":"none"}], # example with extra plot kwd,
+    [1,2, 3, 4],
+    [1,3],
+    [{"x":2,"y":[3,4], "linestyle":"none"}], # example with
+    #Headers example: ['datetime', 'DIFF1_mean[mV]', 'DIFF1_max[mV]', 'DIFF2_mean[mV]', 'DIFF2_max[mV]', 'notes']
+    #[3, 'DIFF1_mean[mV]'],
+        #[{"x":'DIFF1_mean[mV]',"y":['DIFF1_mean[mV]',4], "linestyle":"none"}], # example with extra plot kwd,
+    #[{"x":'DIFF1_mean[mV]',"y":[3,4], "linestyle":"none"}], # example with extra plot kwd,
+    
+                       ]
+##########################################################################
+
+# Create data logger object and run it!
+dl = rtp.SerialDL(port, amount, columns_to_plot, unit)
+dl.run()
+
