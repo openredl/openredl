da5bae62eb6e3104787bb777bda62f55f1740922
res <res@uol.de>
2025-11-06 11:51:44 +0100

    real_time_plot cleaned

--- Firmware/OpenREDL_lib/examples/labs/winter_storage_battery/real_time_plot.py.h
+++ Firmware/OpenREDL_lib/examples/labs/winter_storage_battery/real_time_plot.py.h
@@ -1,26 +1,41 @@
-"""Real Time Plot script addapted for battery storage experiment
-   For execution, remove the extension .h, only necessary for autocopy.
+"""Real Time Plot script template
+   ** THIS IS A PYTHON SCRIPT, not a .h header ** 
+   To execute it:
+   - remove the file name extension .h (only necessary for autocopy)
+   - activate the right environment: conda activate openredl-env 
+   - execute the script: python real_time_plot.py
 """
+
 import pyopenredl.realtimeplotutils as rtp
 
 ##########################################################################
 # CONFIGURATION PARAMETERS
 ##########################################################################
 
-# Automatic port selection.
-terms = ['D307R956', 'arduino'] #terms to search automatically for a port.
-port = rtp.autoselect_port(terms, 0)
-# Else, select manually the port of the Datalogger in your computers.
+#####################################################################
+########## Port Selection ###########################################
+#####################################################################
+# Default is autoselect. Else, select manually the port of the Datalogger in your computers.
 # You can find the right port name in:
 # Arduino Ide > tools > port or in Windows: device manager.
 #port = '/dev/ttyACM0'
 #port = '/dev/rfcomm0'
 #port = 'COM3'
+#terms to search automatically for a port.
+terms = ['D307R956', 'arduino', "2341:0043"]
+# Automatic port selection.
+port = rtp.autoselect_port(terms, 0)
 
+#####################################################################
+########## Plot Length ##############################################
+#####################################################################
 # how many units of data should be plotted
 unit = "seconds" # points/seconds
 amount = 60*60 # how many units should be kept in the plot
 
+#####################################################################
+########## Data to Plot #############################################
+#####################################################################
 # Which data columns should be plotted?
 # Array of subarrays. Each subarray "[]" is a plotting axis.
 # y_axis_n can be given by column_number or column_name
@@ -29,22 +44,25 @@ amount = 60*60 # how many units should be kept in the plot
 #       [y_axis1, y_axis2...]
 #       [{"y":[y_axis1, y_axis2...]}]
 #       [{"x":x_axis, "y":[y_axis1, y_axis2...], "<matplotlib_plot_kwd:value}]
-columns_to_plot = [
-   #[1,2,3,4], # example multiple y_axis with column0/datetime as x_axis
-   #[1],  # example one y_axis with column0/datetime as x_axis
-   #[2, "P1real[W]"], # example with y_axis as column number and as column_name
-   #[{"x":2,"y":[2,"P1real[W]"]}], # example with other column as x _axis
-   #[{"x":2,"y":[3], "linestyle":"none"}], # example with extra plot kwd,
-    [1],
-    [2],
-    [3],
-    #[4],
-    #[5],
-    #[6],
-                   ]
-##########################################################################
+#
+#       [1,2,3,4], # example multiple y_axis with column0/datetime as x_axis
+#       [1],  # example one y_axis with column0/datetime as x_axis
+#       [2, "P1real[W]"], # example with y_axis as column number and as column_name
+#       [{"x":2,"y":[2,"P1real[W]"]}], # example with other column as x _axis
+#       [{"x":2,"y":[3], "linestyle":"none"}], # example with extra plot kwd,
+#       [{"x":2,"y":[3,4], "linestyle":"none"}], # example with
 
-# Create object dl type arduino _serial and run it!
+# Headers example: ['datetime', 'DIFF1_mean[mV]', 'DIFF1_max[mV]', 'DIFF2_mean[mV]', 'DIFF2_max[mV]', 'notes']
+
+columns_to_plot = [
+                    [1],
+                    [2],
+                    [3]
+                   ]
+                   
+#####################################################################
+########## Run the code #############################################
+#####################################################################
+# Create data logger object and run it!
 dl = rtp.SerialDL(port, amount, columns_to_plot, unit)
 dl.run()
-
