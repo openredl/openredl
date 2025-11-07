b7a0fc936a683d9d8c45a6b42f2f0df3ac3614c5
res <res@uol.de>
2025-11-07 13:27:29 +0100

    reformated to profiles

--- Tools/real_time_plot.py
+++ Tools/real_time_plot.py
@@ -1,24 +1,40 @@
+"""Real Time Plot script template
+To execute it:
+- activate the right environment: conda activate openredl-env
+- execute the script: python real_time_plot.py
+"""
+
 import pyopenredl.realtimeplotutils as rtp
 
-##########################################################################
-# CONFIGURATION PARAMETERS
-##########################################################################
-
+#####################################################################
+## Guidelines #######################################################
+#####################################################################
+### 1. Port Selection ###############################################
+#####################################################################
+# Default is autoselect.
+# If autoselect does not work as desired, the port can be selected manually.
+# The right port of the Datalogger in the computer to use have to be given.
+# The port name can be found in:
+# a) Arduino Ide > tools > port
+# b) in Windows: device manager.
+# Common port names include:
+# port = '/dev/ttyACM0'
+# port = '/dev/rfcomm0'
+# port = 'COM3'
+# terms to search automatically for a port.
+terms = ["D307R956", "arduino", "2341:0043"]
 # Automatic port selection.
-#terms to search automatically for a port.
-terms = ['D307R956', 'arduino', "2341:0043"]
 port = rtp.autoselect_port(terms, 0)
-# Else, select manually the port of the Datalogger in your computers.
-# You can find the right port name in:
-# Arduino Ide > tools > port or in Windows: device manager.
-#port = '/dev/ttyACM0'
-#port = '/dev/rfcomm0'
-#port = 'COM3'
-
+#####################################################################
+### 2. Plot Length ##################################################
+#####################################################################
 # how many units of data should be plotted
-unit = "points" # points/seconds
-amount = 1*60 # how many units should be kept in the plot
+# unit = "seconds" # points/seconds
+# amount = 5*60 # how many units should be kept in the plot
 
+#####################################################################
+########## Data to Plot #############################################
+#####################################################################
 # Which data columns should be plotted?
 # Array of subarrays. Each subarray "[]" is a plotting axis.
 # y_axis_n can be given by column_number or column_name
@@ -27,24 +43,65 @@ amount = 1*60 # how many units should be kept in the plot
 #       [y_axis1, y_axis2...]
 #       [{"y":[y_axis1, y_axis2...]}]
 #       [{"x":x_axis, "y":[y_axis1, y_axis2...], "<matplotlib_plot_kwd:value}]
-columns_to_plot = [
-   #[1,2,3,4], # example multiple y_axis with column0/datetime as x_axis
-   #[1],  # example one y_axis with column0/datetime as x_axis
-   #[2, "P1real[W]"], # example with y_axis as column number and as column_name
-   #[{"x":2,"y":[2,"P1real[W]"]}], # example with other column as x _axis
-   #[{"x":2,"y":[3], "linestyle":"none"}], # example with extra plot kwd,
-    [1,2, 3, 4],
-    [1,3],
-    [{"x":2,"y":[3,4], "linestyle":"none"}], # example with
-    #Headers example: ['datetime', 'DIFF1_mean[mV]', 'DIFF1_max[mV]', 'DIFF2_mean[mV]', 'DIFF2_max[mV]', 'notes']
-    #[3, 'DIFF1_mean[mV]'],
-        #[{"x":'DIFF1_mean[mV]',"y":['DIFF1_mean[mV]',4], "linestyle":"none"}], # example with extra plot kwd,
-    #[{"x":'DIFF1_mean[mV]',"y":[3,4], "linestyle":"none"}], # example with extra plot kwd,
-    
-                       ]
-##########################################################################
+#
+#       [1,2,3,4], # example multiple y_axis with column0/datetime as x_axis
+#       [1],  # example one y_axis with column0/datetime as x_axis
+#       [2, "P1real[W]"], # example with y_axis as column number and as column_name
+#       [{"x":2,"y":[2,"P1real[W]"]}], # example with other column as x _axis
+#       [{"x":2,"y":[3], "linestyle":"none"}], # example with extra plot kwd,
+#       [{"x":2,"y":[3,4], "linestyle":"none"}], # example with
+#
+# Headers example: ['datetime', 'DIFF1_mean[mV]', 'DIFF1_max[mV]', 'DIFF2_mean[mV]', 'DIFF2_max[mV]', 'notes']
+# example:
+# columns_to_plot = [
+#                     [1,2],
+#                     [3,4],
+#                     [{"x":2,"y":[3]}]
+#                    ]
 
+#####################################################################
+########## Plotting Profiles Definitions ########################################
+#####################################################################
+
+default_profile = {
+    "unit": "seconds",  # Unit: points or seconds
+    "amount": 5 * 60,  # Amount: how many units should be kept in the plot
+    "columns_to_plot": [[1], [2], [3], [4]],  # Which columns will be plotted per axis
+}
+
+winter_solar_cell_profile = {
+    "unit": "seconds",  # Unit: points or seconds
+    "amount": 10 * 60,  # Amount: how many units should be kept in the plot
+    "columns_to_plot": [
+        # H<datetime,DIFF1_mean[mV],DIFF2_mean[mV],DIFF3_mean[mV],notes>
+        [
+            [1],  # V
+            [2],  # I
+            [3],  # G?
+            [{"x": 1, "y": [2], "linestyle": "none"}],
+        ]
+    ],
+}
+
+winter_storage_battery_profile = {
+    "unit": "seconds",  # Unit: points or seconds
+    "amount": 60 * 60,  # Amount: how many units should be kept in the plot
+    "columns_to_plot":
+    # H<datetime,DIFF1_mean[mV],DIFF2_mean[mV],DIFF3_mean[mV],DIFF4_mean[mV],notes>
+    [[1], [2], [3]],
+}
+
+
+#####################################################################
+########## Run Plotting Code ########################################
+#####################################################################
+# Select the profile to plot
+plot_profile = default_profile
 # Create data logger object and run it!
-dl = rtp.SerialDL(port, amount, columns_to_plot, unit)
+dl = rtp.SerialDL(
+    port=port,
+    points=plot_profile["amount"],
+    cols_to_plot=plot_profile["columns_to_plot"],
+    select=plot_profile["unit"],
+)
 dl.run()
-
